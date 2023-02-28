45cf2d6b14376a96231488bede9cf04e7c631001
delfiponciano12 <poncianodelfina24@gmail.com>
2023-02-28 08:22:20 -0300

    Ejercicio de uso de String

---
+++ Manipula_Cadenas.java
@@ -0,0 +1,27 @@
+
+public class Manipula_Cadenas {
+
+	public static void main(String[] args) {
+		
+		String nombre= "pablito";
+		
+		System.out.println("Mi nombre es " + nombre);
+		
+		System.out.println("Mi nombre tiene " + nombre.length()+ " letras");
+		
+		System.out.println( nombre + " comienza con la letra " + nombre.charAt(0));
+		
+		int ultimaletra;
+		
+		ultimaletra= nombre.length()-1;
+		
+		System.out.println("La ultima letra del nombre " + nombre + " es la " + nombre.charAt(ultimaletra) );
+		
+		
+		
+		
+		
+
+	}
+
+}
